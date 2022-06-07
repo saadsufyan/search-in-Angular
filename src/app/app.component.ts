6c71dc44b3328a0987eba175bc256f2c2da23470
saad <saadsufyan19@gmail.com>
2022-06-07 13:48:11 -0700

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { repos } from './shared/repos';
+import { RestApiService } from './shared/rest-api.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'search-angular-app';
+export class AppComponent   {
+  userName: string = "muzzammil194"
+  repos: repos[] | undefined;
+ 
+  loading: boolean = false;
+  errorMessage: string | undefined;
+ 
+  constructor(private githubService: RestApiService) {
+  }
+ 
+  public getRepos() {
+    this.loading = true;
+    this.errorMessage = "";
+    this.githubService.getRepos(this.userName)
+      .subscribe(
+        (response) => {                           
+          console.log('response received')
+          let x=[];
+          x.push(response)
+          this.repos = x; 
+        },
+        (error) => {                              
+          console.error('Request failed with error')
+          this.errorMessage = error;
+          this.loading = false;
+        },
+        () => {                                   
+          console.error('Request completed')       
+          this.loading = false; 
+        })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatFormFieldModule  } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
